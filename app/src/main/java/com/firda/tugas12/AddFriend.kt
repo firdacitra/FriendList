473f18bf0fd49de5a96c7f2fa2894b0821e7aa5f
firdacitra <firdazahramadan02@gmail.com>
2024-09-06 22:47:12 +0700

    Menambahkan data foto

--- app/src/main/java/com/firda/tugas12/AddFriend.kt
+++ app/src/main/java/com/firda/tugas12/AddFriend.kt
@@ -1,27 +1,96 @@
 package com.firda.tugas12
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-
 import android.widget.Toast
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.firda.tugas12.databinding.ActivityAddFriendBinding
 import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.Base64
 
 class AddFriend : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAddFriendBinding
     private lateinit var viewModel: FriendViewModel
     private var friendId: Int? = null
+    private var photoPath: String = ""
+    private lateinit var photoFile: File
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private var galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val parcelFileDescriptor = contentResolver.openFileDescriptor(
+                    it.data?.data ?: return@registerForActivityResult, "r"
+                )
+                val fileDescriptor = parcelFileDescriptor?.fileDescriptor
+                val inputStream = FileInputStream(fileDescriptor)
+                val outputStream = FileOutputStream(photoFile)
+
+                inputStream.use { input ->
+                    outputStream.use { output ->
+                        input.copyTo(output)
+                    }
+                }
+
+                parcelFileDescriptor?.close()
+                val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+                binding.addImg.setImageBitmap(takenImage)
+                photoPath = bitmapToStr(takenImage)
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_friend)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_friend)
+        enableEdgeToEdge()
 
+        initView()
+
+        photoFile = try {
+            createImageFile()
+        } catch (ex: IOException) {
+            Toast.makeText(this, "Error creating image file", Toast.LENGTH_SHORT).show()
+            return
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "photo_${System.currentTimeMillis()}", // Nama file
+            ".jpg",
+            storageDir
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initView() {
         val factoryModel = Factory(this)
         viewModel = ViewModelProvider(this, factoryModel)[FriendViewModel::class.java]
 
@@ -32,44 +101,67 @@ class AddFriend : AppCompatActivity() {
                 viewModel.getFriend().collect { friends ->
                     val friend = friends.firstOrNull { it.id == friendId }
                     if (friend != null) {
-                        findViewById<EditText>(R.id.ad_nama).setText(friend.name)
-                        findViewById<EditText>(R.id.ad_sekolah).setText(friend.school)
-                        findViewById<EditText>(R.id.ad_hobi).setText(friend.hobby)
+                        binding.adNama.setText(friend.name)
+                        binding.adSekolah.setText(friend.school)
+                        binding.adHobi.setText(friend.hobby)
+
+                        val photoBitmap = strToBitmap(friend.photo)
+                        binding.addImg.setImageBitmap(photoBitmap)
+                        photoPath = friend.photo
                     }
                 }
             }
-            findViewById<Button>(R.id.btn_sv).visibility = View.GONE
-            findViewById<Button>(R.id.update_button).visibility = View.VISIBLE
+            binding.btnSv.visibility = View.GONE
+            binding.updateButton.visibility = View.VISIBLE
+            binding.deleteButton.visibility = View.VISIBLE
         } else {
-            findViewById<Button>(R.id.btn_sv).visibility = View.VISIBLE
-            findViewById<Button>(R.id.update_button).visibility = View.GONE
+            binding.btnSv.visibility = View.VISIBLE
+            binding.updateButton.visibility = View.GONE
+            binding.deleteButton.visibility = View.GONE
         }
 
-        findViewById<Button>(R.id.btn_sv).setOnClickListener {
+        binding.btnSv.setOnClickListener {
             saveFriend()
         }
 
-        findViewById<Button>(R.id.update_button).setOnClickListener {
+        binding.updateButton.setOnClickListener {
             updateFriend()
         }
 
-        findViewById<Button>(R.id.delete_button).setOnClickListener {
-            finish()
+        binding.deleteButton.setOnClickListener {
+            deleteFriend()
+        }
+
+        binding.addImg.setOnClickListener {
+            openGallery()
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(galleryIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun saveFriend() {
-        val name = findViewById<EditText>(R.id.ad_nama).text.toString()
-        val school = findViewById<EditText>(R.id.ad_sekolah).text.toString()
-        val hobby = findViewById<EditText>(R.id.ad_hobi).text.toString()
+        Log.d("AddFriend", "Photo Base64: $photoPath")
+
+        val name = binding.adNama.text.toString()
+        val school = binding.adSekolah.text.toString()
+        val hobby = binding.adHobi.text.toString()
 
         if (name.isBlank() || school.isBlank() || hobby.isBlank()) {
-            Toast.makeText(this, "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show()
             return
         }
 
-        val friend = Friend(name = name, school = school, hobby = hobby)
 
+        val photoPath = bitmapToStr(binding.addImg.drawable.toBitmap())
+        Log.d("AddFriend", "Base64 Photo: $photoPath") // Log untuk memeriksa string Base64
+
+
+        val friend = Friend(name = name, school = school, hobby = hobby, photo = photoPath)
         lifecycleScope.launch {
             viewModel.insertFriend(friend)
             setResult(RESULT_OK)
@@ -77,23 +169,67 @@ class AddFriend : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun updateFriend() {
-        val name = findViewById<EditText>(R.id.ad_nama).text.toString()
-        val school = findViewById<EditText>(R.id.ad_sekolah).text.toString()
-        val hobby = findViewById<EditText>(R.id.ad_hobi).text.toString()
+
+        Log.d("AddFriend", "Photo Base64: $photoPath")
+
+        val name = binding.adNama.text.toString()
+        val school = binding.adSekolah.text.toString()
+        val hobby = binding.adHobi.text.toString()
 
         if (name.isBlank() || school.isBlank() || hobby.isBlank()) {
-            Toast.makeText(this, "Semua field harus diisi", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show()
             return
         }
 
-        val friend = Friend(name = name, school = school, hobby = hobby).apply {
-            id = friendId!!
+        val photoPath = bitmapToStr(binding.addImg.drawable.toBitmap())
+        Log.d("UpdateFriend", "Base64 Photo: $photoPath") // Log untuk memeriksa string Base64
+
+
+        val friend = Friend(name = name, school = school, hobby = hobby, photo = photoPath).apply {
+            id = friendId ?: 0
         }
+
         lifecycleScope.launch {
             viewModel.updateFriend(friend)
             setResult(RESULT_OK)
             finish()
         }
     }
+
+    private fun deleteFriend() {
+        val friend = Friend(name = "", school = "", hobby = "", photo = "").apply {
+            id = friendId ?: 0
+        }
+        lifecycleScope.launch {
+            viewModel.deleteFriend(friend)
+            setResult(RESULT_OK)
+            finish()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun bitmapToStr(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        return Base64.getEncoder().encodeToString(byteArray)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun strToBitmap(encodedString: String): Bitmap? {
+        return try {
+            val cleanedString = encodedString.replace("\\s".toRegex(), "")
+            val byteArray = Base64.getDecoder().decode(cleanedString)
+            Log.d("ImageConversion", "Byte array size: ${byteArray.size}")
+            BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        } catch (e: Exception) {
+            Log.e("ImageConversion", "Error decoding image", e)
+            e.printStackTrace()
+            null
+        }
+    }
+
+
 }
--- app/src/main/java/com/firda/tugas12/Friend.kt
+++ app/src/main/java/com/firda/tugas12/Friend.kt
@@ -4,9 +4,10 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Friend(
-    var name : String,
-    var school : String,
-    var hobby : String
+    var name: String,
+    var school: String,
+    var hobby: String,
+    val photo: String
 ) {
     @PrimaryKey(autoGenerate = true)
     var id : Int = 0
